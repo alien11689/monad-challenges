b4b4bfba4d49c8309b47898b74174446445c633f
Dominik Adam Przybysz <alien11689@gmail.com>
2016-01-20 21:54:27 +0100

    Solved Set1.3

--- src/Set1.hs
+++ src/Set1.hs
@@ -5,13 +5,15 @@ module Set1 where
 
 import MCPrelude
 
+type Gen a = Seed -> (a, Seed)
+
 fiveRands :: [Integer]
 fiveRands = choose 5 [] $ mkSeed 1
     where choose 0 acc _ = acc
           choose amount acc seed = let (new, newSeed) = rand seed 
                                    in choose (amount - 1) (acc ++ [new]) newSeed
 
-randLetter :: Seed -> (Char, Seed)
+randLetter :: Gen Char
 randLetter seed = let (new, newSeed) = rand seed
                   in (toLetter new, newSeed)
 
@@ -20,3 +22,16 @@ randString3 = choose 3 [] $ mkSeed 1
     where choose 0 acc _ = acc
           choose amount acc seed = let (new, newSeed) = randLetter seed
                                    in choose (amount - 1) (acc ++ [new]) newSeed
+
+randEven :: Gen Integer -- the output of rand * 2
+randEven seed = generalA (*2) rand seed
+
+randOdd :: Gen Integer -- the output of rand * 2 + 1
+randOdd = generalA (+1) randEven
+
+randTen :: Gen Integer -- the output of rand * 10
+randTen = generalA (*10) rand
+
+generalA :: (a -> b) -> Gen a -> Gen b
+generalA f gen seed = let (new, newSeed) = gen seed
+                      in (f new, newSeed)
