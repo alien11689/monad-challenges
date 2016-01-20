eb0af7b9190b4d9d7004a6e8546df02f7accba60
Dominik Adam Przybysz <alien11689@gmail.com>
2016-01-20 21:34:57 +0100

    Solved Set1.2

--- src/Set1.hs
+++ src/Set1.hs
@@ -10,3 +10,13 @@ fiveRands = choose 5 [] $ mkSeed 1
     where choose 0 acc _ = acc
           choose amount acc seed = let (new, newSeed) = rand seed 
                                    in choose (amount - 1) (acc ++ [new]) newSeed
+
+randLetter :: Seed -> (Char, Seed)
+randLetter seed = let (new, newSeed) = rand seed
+                  in (toLetter new, newSeed)
+
+randString3 :: String
+randString3 = choose 3 [] $ mkSeed 1
+    where choose 0 acc _ = acc
+          choose amount acc seed = let (new, newSeed) = randLetter seed
+                                   in choose (amount - 1) (acc ++ [new]) newSeed
