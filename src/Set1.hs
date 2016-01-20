cd7104250d45ec93ca0e045efd775979f8ca15fd
Dominik Adam Przybysz <alien11689@gmail.com>
2016-01-20 22:23:22 +0100

    Solve Set1.4

--- src/Set1.hs
+++ src/Set1.hs
@@ -10,7 +10,7 @@ type Gen a = Seed -> (a, Seed)
 fiveRands :: [Integer]
 fiveRands = choose 5 [] $ mkSeed 1
     where choose 0 acc _ = acc
-          choose amount acc seed = let (new, newSeed) = rand seed 
+          choose amount acc seed = let (new, newSeed) = rand seed
                                    in choose (amount - 1) (acc ++ [new]) newSeed
 
 randLetter :: Gen Char
@@ -24,7 +24,7 @@ randString3 = choose 3 [] $ mkSeed 1
                                    in choose (amount - 1) (acc ++ [new]) newSeed
 
 randEven :: Gen Integer -- the output of rand * 2
-randEven seed = generalA (*2) rand seed
+randEven = generalA (*2) rand
 
 randOdd :: Gen Integer -- the output of rand * 2 + 1
 randOdd = generalA (+1) randEven
@@ -35,3 +35,21 @@ randTen = generalA (*10) rand
 generalA :: (a -> b) -> Gen a -> Gen b
 generalA f gen seed = let (new, newSeed) = gen seed
                       in (f new, newSeed)
+
+randPair :: Gen (Char, Integer)
+randPair seed = let (first, newSeed) = randLetter seed
+                    (second, secondSeed) = rand newSeed
+                in ((first, second), secondSeed)
+
+generalPair :: Gen a -> Gen b -> Gen (a, b)
+generalPair ga gb seed = let (first, newSeed) = ga seed
+                             (second, secondSeed) = gb newSeed
+                         in ((first, second), secondSeed)
+
+generalB :: (a -> b -> c) -> Gen a -> Gen b -> Gen c
+generalB f ga gb seed = let (first, newSeed) = ga seed
+                            (second, secondSeed) = gb newSeed
+                        in (f first second, secondSeed)
+
+generalPair2 :: Gen a -> Gen b -> Gen (a, b)
+generalPair2 = generalB (\x y -> (x,y))
