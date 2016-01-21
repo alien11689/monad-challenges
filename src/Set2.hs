ac856857a9d99900354348c5c8ae1e7242dd1781
Dominik Adam Przybysz <alien11689@gmail.com>
2016-01-21 15:51:06 +0100

    Solve Set2.2

--- src/Set2.hs
+++ src/Set2.hs
@@ -10,3 +10,37 @@ data Maybe a = Nothing | Just a
 instance Show a => Show (Maybe a) where
     show Nothing = "Nothing"
     show (Just x) = "Just " ++ show x
+
+headMay :: [a] -> Maybe a
+headMay [] = Nothing
+headMay (x:_) = Just x
+
+tailMay :: [a] -> Maybe [a]
+tailMay [] = Nothing
+tailMay (_:xs) = Just xs
+
+lookupMay :: Eq a => a -> [(a, b)] -> Maybe b
+lookupMay _ [] = Nothing
+lookupMay x ((a,b):ab)
+    | x == a = Just b
+    | otherwise = lookupMay x ab
+
+divMay :: (Eq a, Fractional a) => a -> a -> Maybe a
+divMay _ 0 = Nothing
+divMay x y = Just $ x / y
+
+maximumMay :: Ord a => [a] -> Maybe a
+maximumMay [] = Nothing
+maximumMay (x:xs) = Just $ findMax x xs
+    where findMax cur [] = cur
+          findMax cur (y:ys)
+            | cur < y = findMax y ys
+            | otherwise = findMax cur ys
+
+minimumMay :: Ord a => [a] -> Maybe a
+minimumMay [] = Nothing
+minimumMay (x:xs) = Just $ findMin x xs
+    where findMin cur [] = cur
+          findMin cur (y:ys)
+            | cur > y = findMin y ys
+            | otherwise = findMin cur ys
